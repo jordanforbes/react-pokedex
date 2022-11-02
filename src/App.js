5f0f60fc8b30ab430d81953f5a78afd53548c627
Jordan Forbes <jordforb@gmail.com>
2022-11-02 13:19:51 -0400

    apollo client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+// import { ApolloClient } from 'apollo-boost';
+import { gql, ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client'
+// import { ApolloProvider } from '@apollo/react-hooks';
 
 function App() {
+  const client = new ApolloClient({
+    uri: 'https://beta.pokeapi.co/graphql/v1beta',
+    cache: new InMemoryCache(),
+  })
+
+  const GET_POKEMONS = gql`
+    query pokemons($limit: Int, $offset: Int) {
+      pokemons(limit: $limit, offset: $offset) {
+        count
+        next
+        previous
+        status
+        message
+        results {
+          url
+          name
+          image
+        }
+      }
+    }
+  `
+
+  const gqlVariables = {
+    limit: 2,
+    offset: 1
+  }
+
+  const Todos =()=>{
+    const { loading, error, data} = useQuery(GET_POKEMONS, {
+      variables: gqlVariables,
+    });
+
+      if (loading) return 'Loading...';
+      if (error) return `Error! ${error.message}`
+
+      console.log('Response from server '+ data);
+      return (<p>'Success!'</p>)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todos />
     </div>
   );
 }
